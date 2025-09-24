0d36d5cbd7de4c92d2599083674da043683dc65d
NhatMinh <mcpegunny@gmail.com>
2025-09-24 19:18:30 +0700

    fix user with get page all things

---
+++ src/NekoViBE.Application/Common/DTOs/User/UserFilter.cs
@@ -0,0 +1,38 @@
+﻿using NekoViBE.Application.Common.Models;
+using NekoViBE.Domain.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace NekoViBE.Application.Common.DTOs.User
+{
+    public class UserFilter : BasePaginationFilter
+    {
+        [JsonPropertyName("email")]
+        public string? Email { get; set; }
+
+        [JsonPropertyName("firstName")]
+        public string? FirstName { get; set; }
+
+        [JsonPropertyName("lastName")]
+        public string? LastName { get; set; }
+
+        [JsonPropertyName("phoneNumber")]
+        public string? PhoneNumber { get; set; }
+
+        [JsonPropertyName("status")]
+        public EntityStatusEnum? Status { get; set; }
+
+        [JsonPropertyName("roleId")]
+        public Guid? RoleId { get; set; }
+
+        [JsonPropertyName("userType")]
+        public string? UserType { get; set; } // "Customer", "Staff", "Admin"
+
+        [JsonPropertyName("hasAvatar")]
+        public bool? HasAvatar { get; set; }
+    }
+}
---
+++ src/NekoViBE.Application/Common/DTOs/User/UserItem.cs
@@ -0,0 +1,47 @@
+﻿using NekoViBE.Application.Common.DTOs.Role;
+using NekoViBE.Domain.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace NekoViBE.Application.Common.DTOs.User
+{
+    public class UserItem : BaseResponse
+    {
+        [JsonPropertyName("id")]
+        public Guid Id { get; set; }
+
+        [JsonPropertyName("email")]
+        public string Email { get; set; } = string.Empty;
+
+        [JsonPropertyName("firstName")]
+        public string FirstName { get; set; } = string.Empty;
+
+        [JsonPropertyName("lastName")]
+        public string LastName { get; set; } = string.Empty;
+
+        [JsonPropertyName("phoneNumber")]
+        public string? PhoneNumber { get; set; }
+
+        [JsonPropertyName("avatarPath")]
+        public string? AvatarPath { get; set; }
+
+        [JsonPropertyName("status")]
+        public EntityStatusEnum Status { get; set; }
+
+        [JsonPropertyName("lastLoginAt")]
+        public DateTime? LastLoginAt { get; set; }
+
+        [JsonPropertyName("joiningAt")]
+        public DateTime JoiningAt { get; set; }
+
+        [JsonPropertyName("roles")]
+        public List<RoleInfoDTO> Roles { get; set; } = new List<RoleInfoDTO>();
+
+        [JsonPropertyName("userType")]
+        public string UserType { get; set; } = "User"; // Customer, Staff, Admin
+    }
+}
